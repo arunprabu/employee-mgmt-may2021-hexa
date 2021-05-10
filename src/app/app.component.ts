a1ed46ab345f8d72d4826972e1d2d2171a655fba
Arun Vijayarengan <arunprabu.v@hotmail.com>
2021-05-11 00:54:15 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'employee-mgmt-may2021-hexa';
+}
